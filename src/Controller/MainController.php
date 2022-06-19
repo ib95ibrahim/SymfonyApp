c1c92783160df908e405fb9033292b23adba04f9
ibrahim zeroual <ib.zeroual@outlook.com>
2022-06-19 19:44:34 +0100

    create database and entities

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,4 +18,5 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
 }
